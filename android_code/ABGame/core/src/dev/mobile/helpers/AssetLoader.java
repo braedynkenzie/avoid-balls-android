68d15dead277c3309b014543bdd8259506383157
Braedyn Kenzie <braedyn26@gmail.com>
2017-05-02 15:20:12 -0700

    Fixed bug with AssetLoader, paths to sounds were working incorrectly when launching on Android

--- android_code/ABGame/core/src/dev/mobile/helpers/AssetLoader.java
+++ android_code/ABGame/core/src/dev/mobile/helpers/AssetLoader.java
@@ -15,8 +15,8 @@ public class AssetLoader {
 
     public static void load(){
 
-        ballCollide = Gdx.audio.newSound(Gdx.files.internal("C:\\Users\\Braedyn Kenzie\\Code\\ABGame\\android\\assets\\data\\boop.wav"));
-        ballStartSound = Gdx.audio.newSound(Gdx.files.internal("C:\\Users\\Braedyn Kenzie\\Code\\ABGame\\android\\assets\\data\\ball_start_sound.wav"));
+        ballCollide = Gdx.audio.newSound(Gdx.files.internal("data\\boop.wav"));
+        ballStartSound = Gdx.audio.newSound(Gdx.files.internal("data\\ball_start_sound.wav"));
 
     }
 
